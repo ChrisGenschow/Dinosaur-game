646b7d01ccdfbac5470ec6d9fd51422095f276a1
ChrisGenschow <91728844+ChrisGenschow@users.noreply.github.com>
2022-09-02 01:50:16 +0200

    Updated style and added alert, when you collide with an object
    I've updated the theme, and changed it away from the dino and the bird, and made it a heart and a bone.
    I've also added an alert which triggers, when the heart hits the bone.
    My current issue is that I have to reload the site to restart the game, and it doesn't do so on it's own

--- script.js
+++ script.js
@@ -4,20 +4,35 @@ let block = document.querySelector(".block");
 console.log;
 
 // Eventlisteners
-document.addEventListener('keydown', event => {
+document.addEventListener("keydown", (event) => {
   //console.log("keyevent triggered",event);
-  if (event.code === 'Space') {
-    console.log('Space pressed');
+  if (event.code === "Space") {
+    console.log("Space pressed");
     jump();
   }
-})
+});
 
 /*Create jump function*/
 function jump() {
   /*Tilføj Jump class fra css til character elementet*/
-  character.classList.add("animate");
+  if (character.classList != "animate") {
+    character.classList.add("animate");
+  }
   /*Fjern Jump class fra character efter et delay, så man kan hoppe igen*/
   setTimeout(function () {
     character.classList.remove("animate");
-  }, 500);
+  }, 700);
 }
+
+var checkDead = setInterval(function () {
+  var characterTop = parseInt(
+    window.getComputedStyle(character).getPropertyValue("top")
+  );
+  var blockLeft = parseInt(
+    window.getComputedStyle(block).getPropertyValue("left")
+  );
+  if (blockLeft < 60 && blockLeft > 0 && characterTop >= 284) {
+    block.style.animation = "none";
+    alert("You Died");
+  }
+}, 10);
