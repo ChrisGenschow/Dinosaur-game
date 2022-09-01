6eebb8d40db957ab146933abff66cbf2e8d23ec7
ChrisGenschow <91728844+ChrisGenschow@users.noreply.github.com>
2022-09-01 19:13:50 +0200

    Tilføjede descriptions til elementer
    Tilføjede kommentarer til flere af stepsene

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ let character = document.getElementsByClassName("character");
 let block = document.getElementsByClassName("block");
 /*Create jump function*/
 function jump() {
+  /*Tilføj Jump class fra css til character elementet*/
   character.classList.add("animate");
+  /*Fjern Jump class fra character efter et delay, så man kan hoppe igen*/
   setTimeout(function () {
     character.classList.remove("animate");
   }, 500);
