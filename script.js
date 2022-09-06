7402e66fa6e777fe263780a1bebcfe12639de43e
ChrisGenschow <91728844+ChrisGenschow@users.noreply.github.com>
2022-09-07 01:21:10 +0200

    Added comments to js,  Made it so the animations doesn't run, and started changing code
    Since I don't want the same animations on the bones and the jump on the heart, I've started considering another way of making my character move. Currently watching a tutorial.
    
    Also I added comments in JS to define what I want to do with the code, so that it is easier to have an overview of what's being done

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ let character = document.querySelector(".character");
 let block = document.querySelector(".block");
 console.log;
 
+const scoreElem = document.querySelector("[data-score]");
+const startScreenElem = document.querySelector("[data-start-screen]");
+
 // Eventlisteners
+document.addEventListener("keydown", handleStart, { once: true });
 document.addEventListener("keydown", (event) => {
   //console.log("keyevent triggered",event);
   if (event.code === "Space" && !character.classList.contains("animate")) {
@@ -24,8 +28,9 @@ function jump() {
     console.log("Space Pressed");
   }, 700);
 }
+//Check if your character has collided with the obstacle, if it has then alert the player, that they've died.
 
-let checkDead = setInterval(function () {
+/*let checkDead = setInterval(function () {
   let characterTop = parseInt(
     window.getComputedStyle(character).getPropertyValue("top")
   );
@@ -36,9 +41,18 @@ let checkDead = setInterval(function () {
     block.style.animation = "none";
     alert("You Died");
   }
-}, 10);
+}, 10);*/
 
+/*This will be replaced at some point, so that the alert won't be a thing, but the gamebox, character and obstacle will all
+fade out, and you're left with a black screen.*/
+//A red "You Died" text will fade-in and flow up
+//A retry button will be created.
+//Pressing the retry button makes all elements on the site reset.
+
+//update time, so that we can use it for obstacles and a background if we wanted it.
 let lastTime;
+let speedScale;
+let score;
 function update(time) {
   if (lastTime == null) {
     lastTime = time;
@@ -46,9 +60,25 @@ function update(time) {
     return;
   }
   const delta = time - lastTime;
+  updateScore(delta);
   console.log(delta);
 
   lastTime = time;
   window.requestAnimationFrame(update);
 }
-window.requestAnimationFrame(update);
+//Make a function, which updates your score based on the amount of time played.
+function updateScore(delta) {
+  //Giv spilleren 1 point hvert sekund
+  score += delta * 0.001;
+  //Undgå decimaler.
+  scoreElem.textContent = Math.floor(score);
+}
+
+//Lav en function, så spillet begynder at køre, når man trykker på en knap.
+function handleStart() {
+  lastTime = null;
+  score = 0;
+  speedScale = 1;
+  startScreenElem.classList.add("hide");
+  window.requestAnimationFrame(update);
+}
